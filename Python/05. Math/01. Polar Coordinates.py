c0c3a19bf8a69a27704cce7bff6ae8186e617cdd
MomenAhmed <mo2mination@gmail.com>
2023-06-23 02:23:19 +0300

    Add Polar Coordinates

--- Python/05. Math/01. Polar Coordinates.py
+++ Python/05. Math/01. Polar Coordinates.py
@@ -1,5 +1,5 @@
 import cmath
 
-equation = input()
-print(abs(complex(equation)))
-print(cmath.phase(complex(equation)))
+equation = complex(input().strip())
+print(abs(equation))
+print(cmath.phase(equation))
