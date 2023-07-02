115c8e6d6db37205a9bb9de2544fde894fe76e97
MomenAhmed <mo2mination@gmail.com>
2023-07-02 16:12:03 +0300

    Add Input()

--- Python/11. Built-Ins.py/02. Input().py
+++ Python/11. Built-Ins.py/02. Input().py
@@ -1,30 +1,4 @@
-import re
-x,k = input().split(" ")
-sum_ = 0
-string = input()
-signs = re.findall('[\+\-]',string)
-new_string = re.sub('\-','\+',string)
-
-for i,t in enumerate(new_string.split(' + ')):
-    coef = re.findall(r'\d+(?=x)',t)
-    if len(coef)==0:
-        if re.fullmatch('\d+',t):
-            coef = re.findall('\d+',t)[0]
-        else:
-            coef = 1
-    else:
-        coef = coef[0]
-    
-    power = re.findall(r'(?<=x\*\*)\d',t)
-    if len(power)==0:
-        if re.findall('x',t):
-            power =[1]
-        else:
-            power =[0]
-    
-    if i>0 and signs[i-1]=='-':
-        sum_-= int(str(coef))*(int(str(x))**int(str(power[0])))
-    else:
-        sum_+= int(str(coef))*(int(str(x))**int(str(power[0])))
-                
-print(True if sum_ == int(k) else False)
+P, K = map(int, input().split())
+x = P
+equation = input()
+print('True' if eval(equation) == K else 'False')
