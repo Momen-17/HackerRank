099f782d4a58b977f546fb5ac828962971472c15
MomenAhmed <mo2mination@gmail.com>
2023-07-03 19:56:32 +0300

    Add HTML Parser - Part 1

---
+++ Python/13. Regex and Parsing/11. HTML Parser - Part 1.py
@@ -0,0 +1,16 @@
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print('Start :', tag)
+        [print(f'-> {attr[0]} > {attr[1]}') for attr in attrs]
+    def handle_endtag(self, tag):
+        print('End   :', tag)
+    def handle_startendtag(self, tag, attrs):
+        print('Empty :', tag)
+        [print(f'-> {attr[0]} > {attr[1]}') for attr in attrs]
+
+parser = MyHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
